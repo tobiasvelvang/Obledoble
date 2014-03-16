b37ec35a23b3870f4b91e49a4ec25d10473d8f33
Tobias Velvang <tobiasvelvang@gmail.com>
2014-03-16 20:39:49 +0100

    fixed hit frame

--- Project/Assets/Scripts/GameMaster.cs
+++ Project/Assets/Scripts/GameMaster.cs
@@ -3,6 +3,10 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 	public GameObject cannonObject;
+	public int shootsLeft = 3;
+
+	TextMesh textMesh;
+
 	Cannon cannon;
 
 	// Use this for initialization
@@ -10,24 +14,36 @@ public class GameMaster : MonoBehaviour {
 		cannon = cannonObject.GetComponent<Cannon> ();
 		cannon.onCannonFire += onFire;
 		cannon.canFire = true;
+		textMesh = GetComponent<TextMesh> ();
+		textMesh.text = "Shots left: " + shootsLeft;
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 	public void onFire(object sender, CannonFireEvent args){
 		Debug.Log ("poop");
 		args.projectile.onCollideHandler += onCollide ;
 		cannon.canFire = false;
+		if (shootsLeft == 0) {
+						textMesh.text = "Game over";
+				} else {
+						shootsLeft = shootsLeft - 1;
+						textMesh.text = "Shots left: " + shootsLeft;
+				}
+
+
 	}
 	public void onCollide(object sender, ProjectileEvent args){
 		GameObject other = args.other;
 		if(other.layer == LayerMask.NameToLayer("circles")){
 			Destroy(args.projectile.gameObject);
 			cannon.canFire = true;
-		}
+		}if (shootsLeft == 0) {
+						cannon.canFire = false;
+				}
 
 	}
 
--- Project/Assets/Scripts/Wall.cs
+++ Project/Assets/Scripts/Wall.cs
@@ -8,10 +8,8 @@ public class Wall : MonoBehaviour {
 
 		BoxCollider2D collider = GetComponent<BoxCollider2D>();
 		Vector2 extents = renderer.bounds.extents;
-		if (transform.rotation.z > 0.2) {
-			collider.size = new Vector2(extents.y, extents.x/2);
-		} else {
-			collider.size = new Vector2(extents.x*2, extents.y);
+		if (transform.rotation.z > 0.5) {
+			collider.size = new Vector2(extents.y, extents.x);
 		}
 
 
