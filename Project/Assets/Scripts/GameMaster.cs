584e18e8fb0c79b1842527319b3763dc5ae7b846
Tobias Velvang <tobiasvelvang@gmail.com>
2014-03-16 22:25:58 +0100

    Score counter

--- Project/Assets/Scripts/GameMaster.cs
+++ Project/Assets/Scripts/GameMaster.cs
@@ -3,10 +3,13 @@ using System.Collections;
 
 public class GameMaster : MonoBehaviour {
 	public GameObject cannonObject;
+	public GameObject scoreTextObject;
 	public int shootsLeft = 3;
 	public float timelapsed = 0F;
 	TextMesh textMesh;
 	TextMesh score;
+	TextMesh totalScoreField;
+	float totalScore;
 	public int multiplier = 0;
 
 	Cannon cannon;
@@ -18,18 +21,26 @@ public class GameMaster : MonoBehaviour {
 		cannon.canFire = true;
 		textMesh = GetComponent<TextMesh> ();
 		textMesh.text = "Shots left: " + shootsLeft + "Score: " + (int)timelapsed + "*" + multiplier;
-
+		totalScoreField = scoreTextObject.GetComponent<TextMesh> ();
+		changeScore (0);
 	
 	}
+	void changeScore(float newScore){
+		totalScore = newScore;
+		totalScoreField.text = "" + (int)totalScore;
+
+		}
 	
 	// Update is called once per frame
 	void Update () {
 		if (!(cannon.canFire)) {
 			timelapsed += Time.deltaTime*5;
 				}
-
-		textMesh.text = "Shots left: " + shootsLeft + "Score: " + (int)timelapsed + "*" + multiplier;
-
+		if (shootsLeft == 0) {
+						textMesh.text = "Game over";
+				} else {
+						textMesh.text = "Shots left: " + shootsLeft + "Score: " + (int)timelapsed + "*" + multiplier;
+				}
 
 	}
 	public void onFire(object sender, CannonFireEvent args){
@@ -50,13 +61,16 @@ public class GameMaster : MonoBehaviour {
 		if(other.layer == LayerMask.NameToLayer("circles")){
 			Destroy(args.projectile.gameObject);
 			cannon.canFire = true;
+			changeScore(totalScore + timelapsed * multiplier);
+			timelapsed = 0F;
+			multiplier = 0;
 		
 		}if (other.layer == LayerMask.NameToLayer ("walls")) {
 			multiplier += 1;
 				}
 		if (shootsLeft == 0) {
 						cannon.canFire = false;
-						textMesh.text = "Shots left: " + shootsLeft + "Score: " + (int)timelapsed + "*" + multiplier;
+						textMesh.text = "Game Over";
 		}
 
 	}
