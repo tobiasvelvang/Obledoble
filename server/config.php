d834c1a6fba9baf6d85907ea32f0101e1ecbc3bd
eirikjak <eirikjak@gmail.com>
2014-03-17 03:01:48 +0100

    changes hash secret

--- server/config.php
+++ server/config.php
@@ -3,6 +3,6 @@ $dbname = 'doblescores';
 $user = 'root';
 $password = 'yvvj5746';
 $host = '127.0.0.1';
-$secret = '8bb3cdf465001bc0ab7c97e08f11e78930d0d5ff'; // sha1 on d6QtX55hutcLPSQhIFHB
+$secret = '8bb3cdf465001bc0ab';
 
 ?>
