d17cb70256eb5c66d10bb49d07ea73c9da4bf916
Tobias Velvang <tobiasvelvang@gmail.com>
2014-03-30 16:38:52 +0200

    Removed files resetbutton

--- Project/Assets/Scripts/EndOfGameMenuScript.cs
+++ Project/Assets/Scripts/EndOfGameMenuScript.cs
@@ -16,7 +16,7 @@ public class EndOfGameMenuScript : MonoBehaviour {
 
 	public GUISkin restartSkin;
 	public OnButtonClick onClick;
-
+	public string textFieldString = "text field";
 	void OnGUI () {
 
 		GUI.skin = restartSkin;
@@ -24,7 +24,7 @@ public class EndOfGameMenuScript : MonoBehaviour {
 
 		GUI.Box(new Rect(16,110,270,200), redBubble);
 		
-		// Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
+		GUI.TextField (new Rect (16, 210, 180, 50), textFieldString);
 		if(GUI.Button(new Rect(195,210,90,50), submit)) {
 			Application.LoadLevel(1);
 		}
