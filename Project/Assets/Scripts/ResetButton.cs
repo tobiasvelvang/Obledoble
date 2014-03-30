99f3bc51bbab0acb5be761316471beadc5dd39be
Tobias Velvang <tobiasvelvang@gmail.com>
2014-03-30 14:01:30 +0200

    Fixed some things on the restart menu

--- Project/Assets/Scripts/ResetButton.cs
+++ Project/Assets/Scripts/ResetButton.cs
@@ -9,10 +9,20 @@ public delegate void OnButtonClick(GameObject sender);
 
 
 public class ResetButton : MonoBehaviour {
+	public Texture2D redBubble;
 
-
+	public GUISkin restartSkin;
 	public OnButtonClick onClick;
+	private Rect windowRect = new Rect (30, 170, 200, 200);
+	void OnGUI () {
 
+		GUI.skin = restartSkin;
+		windowRect = GUI.Window (0, windowRect, WindowFunction, redBubble);
+
+		}
+	void WindowFunction (int windowID) {
+		// Draw any Controls inside the window here
+	}
 	void OnMouseUp(){
 		onClick(this.gameObject);
 
