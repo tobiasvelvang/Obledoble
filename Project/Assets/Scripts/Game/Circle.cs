fafacf1cb5ac39e4b03bff90dfc58a519b96d59a
eirikjak <eirikjak@gmail.com>
2014-04-30 20:14:33 +0200

    Consistent radius for small circles, enabling batching

--- Project/Assets/Scripts/Game/Circle.cs
+++ Project/Assets/Scripts/Game/Circle.cs
@@ -4,31 +4,40 @@ using System.Collections;
 public class Circle : MonoBehaviour {
     public float Radius;
     public float AnimationSpeed;
+    private float TargetScale;
     private ColliderAdjuster adjuster;
     void Start() {
+        TargetScale = Radius/renderer.bounds.extents.x;
         Vector2 scale = transform.localScale;
         scale.x = 0;
         scale.y = 0;
         this.transform.localScale = scale;
-        ((CircleCollider2D)collider2D).radius = Radius * 2;
-        Color temp = this.renderer.material.color;
-        temp.a = 0.5f;
-        adjuster = gameObject.GetComponent<ColliderAdjuster>();
        
-
+        adjuster = gameObject.GetComponent<ColliderAdjuster>();
+        StartCoroutine(ScaleUp());
     }
 
 
-
-    void Update() {
-        if (renderer.bounds.extents.x < Radius) {
+    IEnumerator ScaleUp() {
+        CircleCollider2D collider = (CircleCollider2D)collider2D;
+        while (renderer.bounds.extents.x < Radius) {
             Vector2 temp = transform.localScale;
             temp.x += AnimationSpeed * Time.deltaTime;
             temp.y += AnimationSpeed * Time.deltaTime;
             transform.localScale = temp;
-            ((CircleCollider2D)collider2D).radius = renderer.bounds.extents.x / transform.localScale.x;
+            collider.radius = renderer.bounds.extents.x / transform.localScale.x;
             adjuster.Adjust();
+            yield return new WaitForEndOfFrame();
         }
+        Vector2 finalScale = transform.localScale;
+        finalScale.x = TargetScale;
+        finalScale.y = TargetScale;
+        transform.localScale = finalScale;
+        
+      
+    }
+    void Update() {
+        
 
     }
 }
